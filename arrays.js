9b06ffbf0339a66512ff5fde32a49a4c4cc81aed
michael furst <michael@furst.us>
2018-05-25 17:39:03 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,10 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "and skittles"]
+
+function addElementToBeginningOfArray(array, element){
+  return [element, ...array]
+}
+
+function destructivelyAddElementToBeginningOfArray(array, element){
+  array.unshift(element)
+  return array
+}
